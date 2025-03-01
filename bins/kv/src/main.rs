4a8876fbdc61aaec4b7177efd0f93e1f2d46877b
magwoo <magwood51@yandex.ru>
2025-03-02 01:43:17 +0300

    small updates and fixes

--- bins/kv/src/main.rs
+++ bins/kv/src/main.rs
@@ -4,7 +4,13 @@ use rewind_kv::Database;
 fn main() -> anyhow::Result<()> {
     let mut database = Database::new("./test.data".into()).context("failed to open database")?;
 
-    database.set("test", "123").context("failed to set key")?;
+    database
+        .set("key1", "value1")
+        .context("failed to set key")?;
+
+    let value = database.get("key1").context("failed to get value")?;
+
+    println!("value: {:?}", value);
 
     Ok(())
 }
--- libs/kv/src/lib.rs
+++ libs/kv/src/lib.rs
@@ -1,9 +1,8 @@
+use anyhow::Context;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{prelude::*, BufReader};
 use std::io::{Cursor, Seek, SeekFrom};
 use std::path::PathBuf;
-
-use anyhow::Context;
 
 pub struct Database {
     file: File,
@@ -29,7 +28,7 @@ impl Database {
 
         let mut buf = Cursor::new(Vec::new());
 
-        buf.write_all(&(key.len() as u64).to_le_bytes())?;
+        buf.write_all(&(key.len() as u32).to_le_bytes())?;
         buf.write_all(&(value.len() as u64).to_le_bytes())?;
         buf.write_all(key.as_bytes())?;
         buf.write_all(value.as_bytes())?;
@@ -38,6 +37,53 @@ impl Database {
             .write(buf.into_inner().as_ref())
             .context("failed to write to file")?;
 
+        self.file.flush().context("failed to flush file")?;
+
         Ok(())
+    }
+
+    pub fn get(&mut self, key: &str) -> anyhow::Result<Option<String>> {
+        self.file
+            .seek(SeekFrom::Start(0))
+            .context("failed to seek to start")?;
+
+        let mut reader = BufReader::new(&self.file);
+
+        loop {
+            let mut key_len_bytes = [0; size_of::<u32>()];
+            if let Err(e) = reader.read_exact(&mut key_len_bytes) {
+                if e.kind() == std::io::ErrorKind::UnexpectedEof {
+                    return Ok(None);
+                }
+                return Err(e.into());
+            }
+
+            let mut value_len_bytes = [0; size_of::<u64>()];
+            reader
+                .read_exact(&mut value_len_bytes)
+                .context("failed to read value length")?;
+
+            let key_len = u32::from_le_bytes(key_len_bytes) as usize;
+            let value_len = u64::from_le_bytes(value_len_bytes) as usize;
+
+            let mut key_bytes = vec![0; key_len];
+            reader
+                .read_exact(&mut key_bytes)
+                .context("failed to read key bytes")?;
+
+            let record_key =
+                String::from_utf8(key_bytes).context("failed to parse key as UTF-8")?;
+
+            let mut value_bytes = vec![0; value_len];
+            reader
+                .read_exact(&mut value_bytes)
+                .context("failed to read value bytes")?;
+            if record_key == key {
+                let record_value =
+                    String::from_utf8(value_bytes).context("failed to parse value as UTF-8")?;
+
+                return Ok(Some(record_value));
+            }
+        }
     }
 }
